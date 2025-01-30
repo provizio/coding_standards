bc25ec3ad7a9159f318ec65234b1df6a4b23cb35
Ivan Fabek <ivan@provizio.ai>
2025-01-30 18:21:22 +0100

    Set default build type for conan cmake file
    JIRA issue: APT-6650

--- cpp/cmake/Conan.cmake
+++ cpp/cmake/Conan.cmake
@@ -1,5 +1,13 @@
 cmake_minimum_required(VERSION 3.10)
 
+# Release by default
+if(NOT CMAKE_BUILD_TYPE)
+  message(STATUS "Setting build type to 'Release' as none was specified.")
+  set(CMAKE_BUILD_TYPE
+      "Release"
+      CACHE STRING "Choose the type of build." FORCE)
+endif(NOT CMAKE_BUILD_TYPE)
+
 message(STATUS "Executing command: conan install .. --settings=build_type=${CMAKE_BUILD_TYPE} --build=missing -c tools.system.package_manager:mode=install")
 execute_process(COMMAND conan install .. --settings=build_type=${CMAKE_BUILD_TYPE} --build=missing -c tools.system.package_manager:mode=install
                         RESULT_VARIABLE return_code
